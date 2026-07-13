498a4cd974d3fff69299ef7a1876fb1aaabd9715
AnmolGupta912 <pfyplannedforu@gmail.com>
2026-07-13 21:39:35 +0530

    add the login component

--- src/components/index.js
+++ src/components/index.js
@@ -2,11 +2,16 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./Container/Container";
 import Logo from "./Logo";
+import Button from "./Button"
+import Input from "./Input";
+
 
 
 export {
     Header,
     Footer,
     Container,
-    Logo
+    Logo,
+    Button,
+    Input
 }
